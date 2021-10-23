628bb9db521a78f544df5f6a36cfc2193c6feb5c
Somebody_Kseny <skk1020@yandex.ru>
2021-10-23 10:09:31 +0300

    router no any counters

--- router/router.js
+++ router/router.js
@@ -1,37 +1,28 @@
 import { resolve } from "./resolver.js";
 
 class Router {
-    #current = 0;
-
-    #next_uid = 0;
-
     start = (_path, _data) => {
         let view;
         if (view = resolve(_path)) {
-            this.#pushHistoryState(_path, _data);
             this.#changeToView(view);
         }
     }
 
     go = (_path, _data) => {
         if (location.pathname === _path) return;
-        this.#current = this.#next_uid;
-        this.start(_path, _data);
+        let view;
+        if (view = resolve(_path)) {
+            this.#pushHistoryState(_path, _data);
+            this.#changeToView(view);
+        }
     };
 
     popstate(){
-        this.#current = history.state.uid;
-        this.#changeToView(resolve(location.pathname));
+        this.#changeToView(resolve(location.pathname));    
     }
 
     #pushHistoryState = (_path, _data) => {
-        const state = {
-            uid: this.#next_uid,
-            data: _data,
-        }
-        this.#next_uid++;
-        
-        window.history.pushState(state, _path, _path)
+        window.history.pushState(_data, _path, _path)
     }
 
     #changeToView(view){
